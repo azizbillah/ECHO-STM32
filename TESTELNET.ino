f1bfe3937629c53abecd193415f5f62c4f86614b
azizbillah <64129354+azizbillah@users.noreply.github.com>
2022-10-10 07:36:25 +0700

    Update TESTELNET.ino

--- TESTELNET.ino
+++ TESTELNET.ino
@@ -1,135 +1,111 @@
 #include <WiFi.h>
-  
-// debug log, set to 1 to enable
-#define ENABLE_DEBUG_LOG 0
-  
-// wifi config
-const char* ssid     = "NATA";
-const char* password = "11Januari";
-  
+#include <WiFiUdp.h>
+#include <SoftwareSerial.h>
+
+#include "WebServer_WT32_ETH01.h"
+
+#define DEBUG_ETHERNET_WEBSERVER_PORT       Serial
+#define ETH_CLK_MODE_ETH_CLOCK_GPIO0_IN
+#define _ETHERNET_WEBSERVER_LOGLEVEL_       3
+
+SoftwareSerial NMEA(36,15);
+
+const char * ssid = "NATA"; 
+const char * pwd = "11Januari";
+
+
 // ethernet config
 const IPAddress local_IP(192, 168, 1, 98);
 const IPAddress gateway(192, 168, 1, 1);
-const IPAddress subnet(255, 255, 255, 1);
+const IPAddress subnet(255, 255, 255, 0);
 const IPAddress primaryDNS(8, 8, 8, 8);
 const IPAddress secondaryDNS(8, 8, 4, 4);
   
-// rs-server config
-const int serverPort = 1234;
-const int serverPort1 = 1235;
-  
-// rs port config
-const int baudrate = 115200;
-const int rs_config = SERIAL_8N1;
-  
-// reading buffor config
+// IP address to send UDP data to.
+// it can be ip address of the server or 
+// a network broadcast address
+// here is broadcast address
+const char * udpAddress = "192.168.1.4"; // your pc ip
+const int udpPort = 8081; //port server
+const int udpPort1 = 8082; //port server
+
+
+
 #define BUFFER_SIZE 1024
-  
-// global objects
-WiFiServer server, server1;
+//create UDP instance
+WiFiUDP udp, udp1;
 byte buff[BUFFER_SIZE];
-  
-void debug_log(char* str) {
-#if ENABLE_DEBUG_LOG == 1
-   Serial.println(str);
-#endif
-}
-  
+
+
+TaskHandle_t Task1;
+TaskHandle_t Task2;
+
+const int led_1 = 2;
+const int led_2 = 23;
+
 void setup() {
-// init rs port
-   Serial.begin(baudrate, rs_config);
-   Serial2.begin(baudrate, rs_config);
+  Serial.begin(38400); 
+  NMEA.begin(38400);
+  pinMode(led_1, OUTPUT);
+  pinMode(led_2, OUTPUT);
+  WT32_ETH01_onEvent();
+  ETH.begin(ETH_PHY_ADDR, ETH_PHY_POWER);
+  ETH.config(local_IP, gateway, subnet, primaryDNS);
+  WT32_ETH01_waitForConnect();
+//  if (!WiFi.config(local_IP, gateway, subnet, primaryDNS, secondaryDNS)) {
+//      // debug_log("Failed to configure network settings");
+//}
+//   WiFi.begin(ssid, pwd);
+// while (WiFi.status() != WL_CONNECTED) {
+//       //debug_log("connecting to WiFi network");
+//       delay(500);
+//}
+  udp.begin(udpPort);
+  udp1.begin(udpPort1);
+  
+  xTaskCreatePinnedToCore(Task1code,"Task1",10000,NULL,1,&Task1,0);                         
+  //delay(500); 
+  xTaskCreatePinnedToCore(Task2code,"Task2",10000,NULL,1,&Task2,1);          
+  //delay(500);
+}
+
+void Task1code( void * parameter ){
+
+
+  for(;;){
+    int size =0;
+     while((size=NMEA.available()))
+     {
+      size = (size >= BUFFER_SIZE ? BUFFER_SIZE : size);
+      NMEA.readBytes(buff, size);
+      udp1.beginPacket(udpAddress, udpPort1);
+      udp1.write(buff,size);    
+      udp1.endPacket();
+      memset(buff, 0, 50);  
    
-  
-// init wifi connection
-if (!WiFi.config(local_IP, gateway, subnet, primaryDNS, secondaryDNS)) {
-       debug_log("Failed to configure network settings");
-}
-   WiFi.begin(ssid, password);
-while (WiFi.status() != WL_CONNECTED) {
-       debug_log("connecting to WiFi network");
-       delay(500);
-}
-  
-#if ENABLE_DEBUG_LOG == 1
-   Serial.println("connected to WiFi");
-   Serial.println("IP adddr: ");
-   Serial.println(WiFi.localIP());
-#endif
-   delay(1000);
-  
-//start server
-   server = WiFiServer(serverPort);
-   server1 = WiFiServer(serverPort1);
-   server.begin();
-   server1.begin();
-   delay(1000);
-   debug_log("server started");
+    }   
+    vTaskDelay(100);
+  } 
 }
 
+void Task2code( void * parameter ){
 
-void loop(){
-
-Satu();
-Dua();
-  
-}
-void Satu() {
-// wait for client
-   WiFiClient client = server.available();
-if (!client)
-return;
-  
-   debug_log("client found");
-while (client.connected()) {
-int size = 0;
-  
-// read data from wifi client and send to serial
-// while ((size = client.available())) {
-//            size = (size >= BUFFER_SIZE ? BUFFER_SIZE : size);
-//            client.read(buff, size);
-//            Serial.write(buff, size);
-//            Serial.flush();
-// }
-  
-// read data from serial and send to wifi client
-while ((size = Serial.available())) {
-           size = (size >= BUFFER_SIZE ? BUFFER_SIZE : size);
-           Serial.readBytes(buff, size);
-           client.write(buff, size);
-           client.flush();
-}
-}
-   debug_log("client disconnected");
-   client.stop();
+  for(;;){
+     int size =0;
+     while((size=Serial.available()))
+     {
+      size = (size >= BUFFER_SIZE ? BUFFER_SIZE : size);
+      Serial.readBytes(buff, size);
+      udp.beginPacket(udpAddress, udpPort);
+      udp.write(buff,size);    
+      udp.endPacket();
+      memset(buff, 0, 50);  
+   
+    }
+    vTaskDelay(100);
+  }
 }
 
-void Dua() {
-// wait for client
-   WiFiClient client1 = server1.available();
-if (!client1)
-return;
-  
-   debug_log("client found");
-while (client1.connected()) {
-int size = 0;
-  
-// read data from wifi client and send to serial
-while ((size = client1.available())) {
-           size = (size >= BUFFER_SIZE ? BUFFER_SIZE : size);
-           client1.read(buff, size);
-           Serial2.write(buff, size);
-           Serial2.flush();
-}
-  
-// read data from serial and send to wifi client
-while ((size = Serial2.available())) {
-           size = (size >= BUFFER_SIZE ? BUFFER_SIZE : size);
-           Serial2.readBytes(buff, size);
-           client1.write(buff, size);
-           client1.flush();
-}
-}
-   debug_log("client disconnected");
-   client1.stop();
+void loop() {
+
 }
